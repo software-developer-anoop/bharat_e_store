6ee3ef49f32e926f3e065f0c6f7f7fa82ffa3c11
Anoop Maurya <anoopmauryaam02@gmail.com>
2025-05-19 13:15:42 +0530

    Fixed issue for carbon

--- app/Http/Controllers/Api/Notification.php
+++ app/Http/Controllers/Api/Notification.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 class Notification extends Controller
 {
     public function index()
