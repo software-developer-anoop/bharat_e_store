ac0e4a6b37edf8cf8fc383a87641324222e3f48a
Anoop Maurya <anoopmauryaam02@gmail.com>
2025-05-21 16:27:14 +0530

    Fixed owner name according to profile

--- resources/views/backend/layout/component/navbar.blade.php
+++ resources/views/backend/layout/component/navbar.blade.php
@@ -167,7 +167,7 @@
               <div class="media mx-auto">
                 <img src="{{Auth::user()->profile_image?asset('uploads/'.Auth::user()->profile_image):''}}" class="img-fluid mr-2" alt="avatar">
                 <div class="media-body">
-                  <h5>{{$userData->name??''}}</h5>
+                  <h5>{{Auth::user()->name??''}}</h5>
                   <p>Owner</p>
                 </div>
               </div>
