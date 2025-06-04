81599e2475f10f89454d1e5fb3ba70ab01c11e2e
Anoop Maurya <anoopmauryaam02@gmail.com>
2025-06-04 14:24:46 +0530

    Controller for the cmspages

---
+++ app/Http/Controllers/Cmspages.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class Cmspages extends Controller
+{
+    public function index(){
+        $page = DB::table('cms_pages')->where('page_name','Contact Us')->select('page_name','description')->first();
+        return view('cms_page',compact('page'));
+    }
+    public function refund_policy(){
+        $page = DB::table('cms_pages')->where('page_name','Refund Policy')->select('page_name','description')->first();
+        return view('cms_page',compact('page'));
+    }
+    public function t_and_c(){
+        $page = DB::table('cms_pages')->where('page_name','Terms And Conditions')->select('page_name','description')->first();
+        return view('cms_page',compact('page'));
+    }
+}
